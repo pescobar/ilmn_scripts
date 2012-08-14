1dba9717966aa0039b615c698896aac1274b511e
tjhartmann <thartmann@illumina.com>
2012-08-14 10:58:18 -0700

    fixed sge-reinstall script; added comment

--- sge/sge-reinstall.sh
+++ sge/sge-reinstall.sh
@@ -4,14 +4,13 @@ ME=`hostname`
 EXECHOSTS=`qconf -sel`
 
 for TARGETHOST in $EXECHOSTS; do
-	if [ "$ME" == "$TARGETHOST" ]; then
-		echo "Skipping $ME. This is the submission host"
-	else
-		numprocs=`qconf -se $TARGETHOST | \
-			awk '/^processors/ {print $2}'`
-		/opt/rocks/bin/rocks set host boot $TARGETHOST action=install
-		qsub -p 1024 -pe mpi $numprocs -q all.q@$TARGETHOST \
-			/opt/gridengine/examples/jobs/reboot.qsub
-		echo "Set $TARGETHOST for Reinstallation"
-	fi
+  if [ "$ME" == "$TARGETHOST" ]; then
+    echo "Skipping $ME. This is the submission host"
+  else
+  numprocs=`qconf -se $TARGETHOST | awk '/^processors/ {print $2}'` # procs, not slots...
+  /opt/rocks/bin/rocks set host boot $TARGETHOST action=install
+  qsub -p 1024 -pe mpi $numprocs -q all.q@$TARGETHOST \
+    /opt/gridengine/examples/jobs/reboot.qsub
+    echo "Set $TARGETHOST for Reinstallation"
+  fi
 done
