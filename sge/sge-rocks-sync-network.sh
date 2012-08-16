59d4dbec9243f660953195644a379c689acaaa34
Todd Hartmann <thartmann@illumina.com>
2012-08-16 08:57:50 -0700

    yet another reorg - better to build rpms by

--- sge/sge-rocks-sync-network.sh
+++ sge/sge-rocks-sync-network.sh
@@ -3,7 +3,7 @@
 LIST="uscp-prd-lncn-8-9 uscp-prd-lncn-8-10 uscp-prd-lncn-8-11 uscp-prd-lncn-8-12"
 
 for HOST in $LIST; do
-  # submit a blocking job that disables the queue on $HOST
+  # submit a high priority blocking job that disables the queue on $HOST
   qsub -p 1024 -sync y -m b -M thartmann@illumina.com -pe fill_node 12 -q prod-s.q@$HOST restartq.sh
   # sync the network config - after the job completes
   rocks sync host network $HOST
