884dc8b11fe84994cc54c76b531c13052d3875bb
Todd Hartmann <thartmann@illumina.com>
2012-08-28 10:16:54 -0700

    better env handling

--- graphite/sge_queues.py
+++ graphite/sge_queues.py
@@ -16,14 +16,22 @@ except:
   sys.exit(1)
 
 # SGE
-os.environ["SGE_ROOT"] = "/opt/gridengine"
-os.environ["SGE_CELL"] = "default"
-os.environ["SGE_ARCH"] = "lx26-amd64"
-os.environ["SGE_EXECD_PORT"] = "537"
-os.environ["SGE_QMASTER_PORT"] = "536"
+try:
+  '''if this is unset, chances are no other SGE env vars are'''
+  os.environ["SGE_ROOT"]
+except:
+  os.environ["SGE_ROOT"] = "/opt/gridengine"
+  os.environ["SGE_CELL"] = "default"
+  os.environ["SGE_ARCH"] = "lx26-amd64"
+  os.environ["SGE_EXECD_PORT"] = "537"
+  os.environ["SGE_QMASTER_PORT"] = "536"
+
+sge_root = os.environ["SGE_ROOT"]
+sge_cell = os.environ["SGE_CELL"]
+sge_arch = os.environ["SGE_ARCH"]
 
 def parse_qstat(name):
-  p1 = Popen([os.environ["SGE_ROOT"] + "/bin/" + os.environ["SGE_ARCH"] + "/qstat","-g","c"], stdout=PIPE)
+  p1 = Popen([sge_root + "/bin/" + sge_arch + "/qstat","-g","c"], stdout=PIPE)
   p2 = Popen(["grep", name ], stdin=p1.stdout, stdout=PIPE)
   [ queue, cqload, used, res, avail, total, aoacds, cdsue ] = str.split(p2.communicate()[0])
 
@@ -37,7 +45,7 @@ while True:
   hostname = os.environ["HOSTNAME"]
   hostname = hostname.replace(".","_")
   now = int( time.time() )
-  queues = str.split(Popen([os.environ["SGE_ROOT"] + "/bin/" + os.environ["SGE_ARCH"] + "/qconf","-sql"], stdout=PIPE).communicate()[0])
+  queues = str.split(Popen([sge_root + "/bin/" + sge_arch + "/qconf","-sql"], stdout=PIPE).communicate()[0])
   for q in queues:
     lines = []
     (cqload, used, res, avail, total) = parse_qstat(q)
