e9727940db546aff633d2f119325df9792ddc4a9
Todd Hartmann <thartmann@illumina.com>
2012-08-29 15:38:03 -0700

    bugfix: eth0 is not always the primary/install interface

--- warewulf/rocks-to-ww.py
+++ warewulf/rocks-to-ww.py
@@ -1,6 +1,7 @@
 from subprocess import Popen,PIPE
 import re
 
+
 def parse_rockscmd():
   """ parse rocks command output """
   rocks_cmd = str.split("rocks list host interface")
@@ -14,30 +15,33 @@ def parse_rockscmd():
   d1 = {} 
   hostdata = []
   for line in output:
-    m = re.match(r'(.*?):\s+\w+\s+(eth\d)\s+(..:..:..:..:..:..)\s+(\d+?\.\d+?\.\d+?\.\d+?)\s+(\d+?\.\d+?\.\d+?\.\d+?)', line)
+    m = re.match(r'(.*?):\s+(\w+)\s+(eth\d)\s+(..:..:..:..:..:..)\s+(\d+?\.\d+?\.\d+?\.\d+?)\s+(\d+?\.\d+?\.\d+?\.\d+?)', line)
     if m:
-      if m.group(2) == "eth0":
-        hostdata = [m.group(2), m.group(3), m.group(4), m.group(5)]
+      if m.group(2) == "private":
+        hostdata = [m.group(2), m.group(3), m.group(4), m.group(5), m.group(6)]
       else:
         hostdata.append(m.group(2))
         hostdata.append(m.group(3))
         hostdata.append(m.group(4))
         hostdata.append(m.group(5))
+        hostdata.append(m.group(6))
       d1[m.group(1)] = hostdata
 
   return(d1)
 
 if __name__ == '__main__':
-  #print "wwsh node new %s --netdev=%s --hwaddr=%s --ipaddr=%s --netmask=%s" % (host,iface,mac,ip,nm)
   d2 = parse_rockscmd()
   i = 0
   for hostname,data in d2.iteritems():
     while data:
+      subnet = data.pop(0)
       netdev = data.pop(0)
       hwaddr = data.pop(0)
       ipaddr = data.pop(0)
       netmask = data.pop(0)
-      if netdev == "eth0":
+      if subnet == "private":
         print "wwsh node new %s --netdev=%s --hwaddr=%s --ipaddr=%s --netmask=%s" % (hostname,netdev,hwaddr,ipaddr,netmask)
       else:
         print "wwsh node set %s --netdev=%s --hwaddr=%s --ipaddr=%s --netmask=%s" % (hostname,netdev,hwaddr,ipaddr,netmask)
+      #elif subnet == "-------":
+      #  pass
