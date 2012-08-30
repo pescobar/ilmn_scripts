7051c556267100fad38d4110a54bcfaabac85d8e
Todd Hartmann <thartmann@illumina.com>
2012-08-30 13:02:37 -0700

    cleaned up and added comments

--- warewulf/rocks-to-ww.py
+++ warewulf/rocks-to-ww.py
@@ -2,7 +2,7 @@ from subprocess import Popen,PIPE
 import re
 
 def parse_rockscmd():
-  """ parse rocks command output """
+  """ parse rocks list host interface command output """
   rocks_cmd = str.split("rocks list host interface")
 
   try:
@@ -15,40 +15,37 @@ def parse_rockscmd():
   data = []
   for line in output:
     m = re.match(r'(.*?):\s+(\w+)\s+(eth\d)\s+(..:..:..:..:..:..)\s+(\d+?\.\d+?\.\d+?\.\d+?)\s+(\d+?\.\d+?\.\d+?\.\d+?)', line)
+    # hostname, subnet, netdev, mac, ip, netmask
     if m:
-      if not m.group(1) in dict.keys(rocks_host_interface): # haven't seen this host before
-        data = [m.group(2), m.group(3), m.group(4), m.group(5), m.group(6)]
+      if not m.group(1) in dict.keys(rocks_host_interface):                 # first time we've seen this host
+        data = [m.group(2), m.group(3), m.group(4), m.group(5), m.group(6)] # initialize the data list
       else:
         data.append(m.group(2))
         data.append(m.group(3))
         data.append(m.group(4))
         data.append(m.group(5))
         data.append(m.group(6))
+
+      # key is host name, value is a list of data for all interfaces
       rocks_host_interface[m.group(1)] = data
   return(rocks_host_interface)
 
 if __name__ == '__main__':
   host_data = parse_rockscmd()
   for host,data in host_data.iteritems():
-    d2 = {}
+    subnet_data = {}
     while data:
-      # how about a dict here that keys off of subnet name?
-      # values are the list of remaining data
-      # then we can pull out the data using the key - 'private' etc.
       sn = data.pop(0)
       nd = data.pop(0)
       hw = data.pop(0)
       ip = data.pop(0)
       nm = data.pop(0)
-      net_data = [nd,hw,ip,nm]
-      d2[sn] = net_data 
-      #print "new subnet '%s' found for host %s" % (subnet,host)
-      #print net_data
-      # pull out the private subnet data to create the ww node
-    (netdev,hwaddr,ipaddr,netmask) = d2["private"]
+
+      # key is the subnet name, value is list [netdev,mac,ip,netmask]
+      subnet_data[sn] = [nd,hw,ip,nm]
+    (netdev,hwaddr,ipaddr,netmask) = subnet_data["private"]
     print "wwsh node new %s --netdev=%s --hwaddr=%s --ipaddr=%s --netmask=%s" % (host,netdev,hwaddr,ipaddr,netmask)
-    for k,v in d2.iteritems():
-      if not k == "private":
-        # next if key is "private"
-        (netdev,hwaddr,ipaddr,netmask) = v
+    for subnet,sn_values in subnet_data.iteritems():
+      if not subnet == "private":
+        (netdev,hwaddr,ipaddr,netmask) = sn_values
         print "wwsh node set %s --netdev=%s --hwaddr=%s --ipaddr=%s --netmask=%s" % (host,netdev,hwaddr,ipaddr,netmask)
