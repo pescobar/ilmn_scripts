ca7435fe779bd09c7ee2ffac34037d3124861815
Todd Hartmann <thartmann@illumina.com>
2012-11-13 13:21:18 -0800

    fixed stdin problem

--- sge/sge_check.py
+++ sge/sge_check.py
@@ -14,6 +14,7 @@ os.environ["SGE_EXECD_PORT"] = "537"
 os.environ["SGE_QMASTER_PORT"] = "536"
 
 
+# check_mk_agent closes stdin which breaks this python construct - how to get around that?
 def parse_qstat(queue_state):
   if(queue_state == "disabled"):
     p1 = Popen([os.environ["SGE_ROOT"] + "/bin/" + os.environ["SGE_ARCH"] + "/qstat","-f","-qs","d"], stdout=PIPE)
@@ -21,21 +22,32 @@ def parse_qstat(queue_state):
     p1 = Popen([os.environ["SGE_ROOT"] + "/bin/" + os.environ["SGE_ARCH"] + "/qstat","-f","-qs","a"], stdout=PIPE)
   else:
     return
-  p2 = Popen(["awk", "/lx/{print $1}"], stdin=p1.stdout, stdout=PIPE)
-  output2 = p2.communicate()[0]
-  
-  return (re.sub(r'\.lo.*', '', output2))
 
-disabled=parse_qstat("disabled")
-dqueue=disabled.split()
+  output = p1.communicate()[0]
 
-alarm=parse_qstat("alarm")
-aqueue=alarm.split()
+  # output string looks something like:
+  # devel.q@ussd-prd-lncn-2-14.loc BIP   0/0/8          0.00     lx26-amd64    d
+
+  match = re.search(r'.*lx26-amd64.*', output)
+  if match:
+    queue_list = re.sub(r'\.lo.*', '', match.group(0))
+    #return (re.sub(r'\@', '_', queue_list))
+    return (queue_list)
+  else:
+    return
+
+
 
 # check output is of the form: status item_name performance_data check_output
 # where status: 0 = OK, 1 = WARNING, 2 = CRITICAL, 3 = UNKNOWN
-for queue in dqueue:
-  print "1 SGE_disabled-%s - SGE queue %s is disabled" % (queue,queue)
+disabled = parse_qstat("disabled")
+if disabled:
+  dqueue = disabled.split()
+  for queue in dqueue:
+    print "1 SGE_disabled-%s - SGE queue %s is disabled" % (queue,queue)
 
-for queue in aqueue:
-  print "2 SGE_alarm-%s - SGE queue %s is in alarm" % (queue,queue)
+alarm = parse_qstat("alarm")
+if alarm:
+  aqueue= alarm.split()
+  for queue in aqueue:
+    print "2 SGE_alarm-%s - SGE queue %s is in alarm" % (queue,queue)
