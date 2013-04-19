5956a057ee1e6dfffa2fa4e7c5ba17ad3dacf38a
Todd Hartmann <thartmann@illumina.com>
2013-04-19 14:23:07 -0700

    added pure python discovery module for cobbler

---
+++ cobbler/install_firstboot_discover.py
@@ -0,0 +1,38 @@
+import re
+
+def register():
+  return "/var/lib/cobbler/triggers/install/firstboot/*"
+
+def run(api, args, logger):
+  objtype = args[0]
+  name    = args[1]
+  ip      = args[2]
+
+
+  # this assumes nodes are alphabetically sorted in h.systems()
+  # appears to be a valid assumption, but may change in future
+  stack = []
+  for system in api.systems():
+    m = re.match(r'node(\d+)', system.name)
+    if m:
+      incr = int(m.group(1)) + 1
+      sysname = "node" + str(incr)
+      stack.append(sysname)
+
+  if len(stack) > 0:
+    newname = stack.pop()
+  else:
+    newname = "node1"
+
+  fqdn = newname + ".foo.illumina.com"
+
+  system = api.find_system(name)
+  system.set_hostname(newname)
+  system.set_dns_name(fqdn,'eth0')
+  system.set_comment('firstboot_trigger')
+  system.set_netboot_enabled('True')
+  api.rename_system(system,newname)
+  api.serialize()
+  return 0
+
+
