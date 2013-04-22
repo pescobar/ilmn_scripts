9c4ef701a6e3b84433defbbcb39b4740acbe6a72
Todd Hartmann <thartmann@illumina.com>
2013-04-22 13:15:25 -0700

    automated node naming and ip addressing

--- cobbler/install_firstboot_discover.py
+++ cobbler/install_firstboot_discover.py
@@ -1,6 +1,11 @@
 import re
+import netaddr
+
+# only user-defined value
+provisioning_subnet = netaddr.IPNetwork('10.1.1.0/24')
 
 def register():
+  # run when called as install/firstboot
   return "/var/lib/cobbler/triggers/install/firstboot/*"
 
 def run(api, args, logger):
@@ -9,39 +14,48 @@ def run(api, args, logger):
   bootip  = args[2]
 
   #########################################################
-  # generate a new node name
+  # identify a new node name and IP address
   #########################################################
-  # this assumes nodes are alphabetically sorted in h.systems()
-  # appears to be a valid assumption, but may change in future
-  stack = []
+  nodes = []
+  ip = []
+
   for system in api.systems():
     m = re.match(r'node(\d+)', system.name)
     if m:
-      incr = int(m.group(1)) + 1
-      sysname = "node" + str(incr)
-      stack.append(sysname)
+      nodes.append(int(m.group(1)))
+      ip.append(netaddr.IPAddress(system.get_ip_address("eth0")))
 
-  if len(stack) > 0:
-    newname = stack.pop()
+  if len(nodes) > 0:
+    nodes.sort()
+    incr = nodes.pop() + 1
+    newname = "node" + str(incr)
   else:
     newname = "node1"
 
   fqdn = newname + ".foo.illumina.com"
 
-  ########################################################
-  # assign a static IP address
-  ########################################################
-  # TODO
+  if len(ip) > 0:
+    ip.sort()
+    ip.reverse()
+    last  = ip.pop()
+    newip = str(last.__isub__(1))
+  else:
+    newip = "10.1.1.254"
 
+  netmask = str(provisioning_subnet.netmask)
+  gateway = str(provisioning_subnet[1])
 
   ###############################################
   # configure the new node
-  # and set it to boot into kickstart
+  # and set it to netboot into kickstart
   ###############################################
   system = api.find_system(name)
   system.set_hostname(fqdn)
   system.set_dns_name(fqdn,'eth0')
-  system.set_comment('firstboot_trigger')
+  system.set_ip_address(newip,'eth0')
+  system.set_netmask(netmask,'eth0')
+  system.set_gateway(gateway)
+  system.set_static('True','eth0')
   system.set_netboot_enabled('True')
   api.rename_system(system,fqdn)
   api.serialize()
