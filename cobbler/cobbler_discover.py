2b2a54201e584a88326b30d2e68dba87c63cae62
Todd Hartmann <thartmann@illumina.com>
2013-05-21 15:41:52 -0700

    add cobbler_discover - wwnodescan for cobbler

---
+++ cobbler/cobbler_discover.py
@@ -0,0 +1,91 @@
+#!/bin/env python
+
+###############################################################################
+# re-implementation of insert-ethers for cobbler
+#   goal is to run this to do node discovery similar to insert-ethers on rocks
+###############################################################################
+import re
+import sys
+import netaddr
+import logging
+logging.getLogger("scapy.runtime").setLevel(logging.ERROR)
+from scapy.all import sniff,DHCP,Ether,IP,BOOTP
+import cobbler.api
+
+api = cobbler.api.BootAPI()
+
+def dhcp_callback(pkt):
+  # does cobbler know about this MAC address?
+  #   yes - do nothing
+  #   no  - add the node to cobbler using the basename and profile specified
+
+  # RFC2132 DHCPDISCOVER == 1
+  if pkt[DHCP] and pkt[DHCP].options[0][1] == 1:
+    mymac = pkt[Ether].src
+
+    # list all known MAC and IP addresses
+    mac = []
+    ip  = []
+    for system in api.systems():
+      mac.append(system.get_mac_address("eth0"))
+      ip.append(netaddr.IPAddress(system.get_ip_address("eth0")))
+
+    # find the next available IP in the range
+    provisioning_subnet = "10.1.1.0/24" # temp hard code for testing
+    if len(ip) > 0:
+      ip.sort()
+      ip.reverse()
+      last  = ip.pop()
+      newip = str(last.__isub__(1))
+    else:
+      newip = str(list(provisioning_subnet).pop().__isub__(1))
+
+    if mymac in mac:
+      print "duplicate mac found; next IP is %s" % newip
+    else:
+      print "mac not found, adding system with IP %s" % newip
+      # TODO: determine uniqueName and uniqueIP, netmask, dns-name, gateway
+      #cobbler system add --name=uniqueName --netboot-enabled=true --hostname=uniqueName --gateway=fromdhcp --mac-address=mymac --ip-address=uniqueIP --netmask=fromdhcp --dns-name=fromdhcp --interface=eth0
+
+  # RFC2132 DHCPOFFER == 2
+  # Pull the DHCP server, router, and subnet mask from the offer
+  # should be other ways to get this...like cobbler api queries? or parsing /etc/dhcpd/dhcpd.conf...
+  #if pkt[DHCP] and pkt[DHCP].options[0][1] == 2:
+  #  dhcp_server = pkt[IP].src
+  #  netmask     = pkt[DHCP].options[3][1]
+  #  gateway     = pkt[DHCP].options[4][1]
+  #  subdomain   = pkt[DHCP].options[6][1]
+
+
+  return
+
+  #########################################################
+  # identify a new node name and IP address
+  #########################################################
+  sn = dhcp_server + '/24'
+  provisioning_subnet = netaddr.IPNetwork(sn)
+
+  for system in api.systems():
+    m = re.match(r'node(\d+)', system.name)
+    if m:
+      nodes.append(int(m.group(1)))
+      ip.append(netaddr.IPAddress(system.get_ip_address("eth0")))
+
+  ###############################################
+  # configure the new node in cobbler
+  # and set it to netboot into kickstart
+  ###############################################
+  #system = api.find_system(name)
+  #system.set_hostname(fqdn)
+  #system.set_dns_name(fqdn,'eth0')
+  #system.set_ip_address(newip,'eth0')
+  #system.set_netmask(netmask,'eth0')
+  #system.set_gateway(gateway)
+  #system.set_static('True','eth0')
+  #system.set_netboot_enabled('True')
+  #api.rename_system(system,fqdn)
+  #api.serialize()
+  #api.sync() 
+
+# sniff dhcp traffic
+sniff(prn=dhcp_callback,filter="((port 67 or 68) and (udp[8:1] = 0x1))", store=0, count=200)
