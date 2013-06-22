808c7dcac9f7d07d69d13120b81d2fd26cf18c66
Todd Hartmann <thartmann@illumina.com>
2013-06-21 19:37:25 -0700

    updated rocks bond script

--- rocks/create-bonds.sh
+++ rocks/create-bonds.sh
@@ -7,12 +7,13 @@ ISIZONE="ussd-prd-isi02a"
 for i in `seq 1 20`; do
 	# gigabit ethernet - use the IP set by rocks for the private interface
 	IP=`rocks list host interface ${BASENAME}${i} | awk '/private/{print $4}'`
-	rocks add host bonded ${BASENAME}${i} channel=bond1 interfaces=eth2,eth3 ip=$IP network=private
-	rocks set host interface options ${BASENAME}${i} bond1 options="miimon=100 mode=0"
+	#rocks add host bonded ${BASENAME}${i} channel=bond1 interfaces=eth2,eth3 ip=$IP network=private
+	#rocks set host interface options ${BASENAME}${i} bond1 options="miimon=100 mode=0"
 
 	# Add 10GbE bonded interfaces
-	#rocks add host bonded ${BASENAME}${i} channel=bond0 interfaces=eth0,eth1 ip=${TENGIG}.${i} network=${ISIZONE}
-	#rocks set host interface options ${BASENAME}${i} bond0 options="miimon=100 mode=0"
+        LAST=`echo $IP | awk -F '.' '{print $4}'`
+        #rocks add host bonded ${BASENAME}${i} channel=bond0 interfaces=eth0,eth1 ip=${TENGIG}.${LAST} network=${ISIZONE}
+        #rocks set host interface options ${BASENAME}${i} bond0 options="miimon=100 mode=0"
 
 	# Assign 10GbE bonded interfaces to Isilon zones
 	#rocks set host interface subnet ${BASENAME}${i} bond0 ${ISIZONE}
