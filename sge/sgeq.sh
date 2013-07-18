67f2381a3baddf581f3b8a5d56d43f03029f1ca3
Todd Hartmann <thartmann@illumina.com>
2013-07-18 12:53:15 -0700

    keep it stupid simple - we just need error state checking

--- sge/sgeq.sh
+++ sge/sgeq.sh
@@ -1,25 +1,9 @@
 #!/bin/bash
 . /etc/profile.d/sge-binaries.sh
 
-# single check for all disabled queues
-# more efficient than inventorying all queue instances - e.g. uscp
-DISABLED=`qstat -f -qs d | awk '/amd64/{print $1}' | sed 's/\.lo.*//'`
-if [ -n "$DISABLED" ]; then
-  for q in $DISABLED; do
-    echo "1 SGE_disabled - SGE queue is disabled"
-  done
-fi
-
-ALARM=`qstat -f -qs a | awk '/amd64/{print $1}' | sed 's/\.lo.*//'`
-if [ -n "$ALARM" ]; then
-  for q in $ALARM; do
-    echo "1 SGE_alarm - SGE queue is in alarm"
-  done
-fi
-
 ERROR=`qstat -f -qs E | awk '/amd64/{print $1}' | sed 's/\.lo.*//'`
 if [ -n "$ERROR" ]; then
   for q in $ERROR; do
-    echo "1 SGE_alarm - SGE queue is in error"
+    echo "1 SGE_alarm - $ERROR is in error"
   done
 fi
