489b5e06d4fe08c3969c74e63e9ddbf6e35fccc1
Todd Hartmann <thartmann@illumina.com>
2013-08-23 15:05:45 -0700

    fleshing out bright node-add script

--- bright/add-cseries.py
+++ bright/add-cseries.py
@@ -7,14 +7,13 @@ import pythoncm
 mgmt_subnet = '10.12.59'
 ipmi_subnet = '10.12.60'
 data_subnet = '10.129.3'
+isilon_zone = 'ussd-prd-isi01b'
 basename = 'ussd-tst-lncn'
 category = 'ilmn-cseries'
-isilon_zone = 'ussd-prd-isi01b'
-
-# Create an instance of ClusterManager, only one is needed
-clustermanager = pythoncm.ClusterManager()
+rack = 'B5'
 
 # Add connection to your cluster using cmsh certificate
+clustermanager = pythoncm.ClusterManager()
 cluster = clustermanager.addCluster('https://localhost:8081', '/cm/local/apps/cmd/etc/cert.pem', '/cm/local/apps/cmd/etc/cert.key');
 
 if not cluster.connect():
@@ -25,11 +24,8 @@ if not cluster.connect():
 internalnet = cluster.find('internalnet')
 datanet = cluster.find(isilon_zone)
 ipminet = cluster.find('ipminet')
-if not ipminet:
-  print "No ipminet found, not adding ipmi0 interfaces"
   
-#for i in range(1, 16):
-for i in range(4, 5):
+for i in range(1, 16):
   node = pythoncm.PhysicalNode()
   cluster.add(node)
   node.hostname = basename + '%02d' % i
@@ -37,22 +33,26 @@ for i in range(4, 5):
   node.category = cluster.find(category, 'Category')
 
   # When creating objects belonging to node, add them as soon as possible
+  # add all four interfaces - this is required to create bonds
   node.interfaces = [pythoncm.NetworkPhysicalInterface()]
-  if ipminet:
-    node.interfaces = node.interfaces + [pythoncm.NetworkBmcInterface()]
   node.interfaces[0].name = 'eth0'
-  node.interfaces[0].ip = mgmt_subnet + '.%d' % (i + 200) 
-  node.interfaces[0].network = internalnet
-  if ipminet:
-    node.interfaces[1].name = 'ipmi0'
-    node.interfaces[1].ip = ipmi_subnet + '.%d' % (i + 200) 
-    node.interfaces[1].network = ipminet
   node.interfaces = node.interfaces + [pythoncm.NetworkPhysicalInterface()]
-  node.interfaces[2].name = 'eth1'
+  node.interfaces[1].name = 'eth1'
   node.interfaces = node.interfaces + [pythoncm.NetworkPhysicalInterface()]
-  node.interfaces[3].name = 'eth2'
+  node.interfaces[2].name = 'eth2'
   node.interfaces = node.interfaces + [pythoncm.NetworkPhysicalInterface()]
-  node.interfaces[4].name = 'eth3'
+  node.interfaces[3].name = 'eth3'
+
+  # bonded interfaces
+  node.interfaces = node.interfaces + [pythoncm.NetworkBondInterface()]
+  node.interfaces[4].name = 'bond0'
+  node.interfaces[4].ip = mgmt_subnet + '.%d' % (i +200)
+  node.interfaces[4].network = internalnet
+  node.interfaces[4].mode = 1
+  node.interfaces[4].interfaces = ['eth0','eth1']
+  # bug in CentOS - can't supply options for provisioning interface (workaround in finalize script)
+  #node.interfaces[4].options = 'miimon=100'
+
   node.interfaces = node.interfaces + [pythoncm.NetworkBondInterface()]
   node.interfaces[5].name = 'bond1'
   node.interfaces[5].ip = data_subnet + '.%d' % (i +200)
@@ -60,14 +60,26 @@ for i in range(4, 5):
   node.interfaces[5].network = datanet
   node.interfaces[5].interfaces = ['eth2','eth3']
   node.interfaces[5].options = 'miimon=100'
-  node.provisioningInterface = node.interfaces[0]
+
+  # ipmi
+  node.interfaces = node.interfaces + [pythoncm.NetworkBmcInterface()]
+  node.interfaces[6].name = 'ipmi0'
+  node.interfaces[6].ip = ipmi_subnet + '.%d' % (i) 
+  node.interfaces[6].network = ipminet
+
+  # provision using bond0
+  node.provisioningInterface = node.interfaces[4]
 
   # set the rack position
-  node.rack = cluster.find('B5')
+  node.rack = cluster.find(rack)
   node.rackPosition = i
   node.rackHeight = 1
 
+  # default is apc if not specified
+  node.powerControl = 'ipmi0'
+
   # set the switch port
+  # requires that Bright is able to walk the switch *that the provisioning interface* is attached to
   #node.ethernetSwitch.ethernetSwitch = cluster.find('switch01')
   #node.ethernetSwitch.port = i
 
