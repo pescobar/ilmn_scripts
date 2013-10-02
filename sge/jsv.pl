84ff25555c05b9c5c733f017b99cea6f99c95c84
Todd Hartmann <thartmann@illumina.com>
2013-10-02 13:32:23 -0700

    gettin there

--- sge/jsv.pl
+++ sge/jsv.pl
@@ -8,13 +8,13 @@ use Env qw(SGE_ROOT);
 use lib "$SGE_ROOT/util/resources/jsv";
 use JSV qw( :ALL );
 
-my $debug = 1; # true: verose logging and all jobs are rejected
-my $mem_threshold = 128; # threshold for large memory jobs (GB)
-my $slot_threshold = 12; # threshold for large parallel jobs (slots)
-my $mem_multiplier = 4; # set h_vmem = $mem_multiplier * slots (GB) for jobs with no mem limit
-my $qlogin_memory_limit = 4; # (GB)
-my $qlogin_time_limit = 24; # (hours)
-my @exclude = qw/sqa.q gpu.q/;
+my $mem_threshold = 128;        # threshold for large memory jobs (GB)
+my $slot_threshold = 12;        # threshold for large parallel jobs (slots)
+my $mem_multiplier = 4;         # jobs with no requested mem limit will get multiplier X slots (GB)
+my $qlogin_mem_limit = 4;       # memory limit for qlogins (GB)
+my $qlogin_time_limit = 24;     # wallclock time limit for qlogins (hours)
+my @exempt_q = qw/sqa.q gpu.q/; # jobs submitted to these queues are passed untouched
+my $debug = 1;                  # true: verbose logging and all jobs are rejected
 
 jsv_on_start(sub {
   jsv_send_env();
@@ -31,7 +31,7 @@ jsv_on_verify(sub {
   ###########################################
   # Queues excluded from JSV processing
   ###########################################
-  for(@exclude){
+  for(@exempt_q){
     if (jsv_sub_is_param('q_hard', $_)){
       # queue is excluded from JSV - submit job as is
       if ($debug) {
@@ -43,11 +43,11 @@ jsv_on_verify(sub {
       }
     }
   }
+
   ##################################################################################
   # Qlogin
   #   all qlogins go to the slice.q
-  #   strip out any -pe requests and set hard run time and memory limits
-  #   TODO: what about special queues like sqa, gpu, etc.? An exclude list maybe?
+  #   strip out any PE requests and set hard wallclock and memory limits
   ##################################################################################
   if ($params{CLIENT} eq "qlogin") {
     if (jsv_is_param('pe_name')){
@@ -59,8 +59,8 @@ jsv_on_verify(sub {
     jsv_sub_del_param('l_hard', 'h_rt') if (jsv_sub_is_param('l_hard','h_rt'));
     jsv_set_param('q_hard', 'slice.q');
     jsv_sub_add_param('l_hard', "h_rt=$qlogin_time_limit:0:0");
-    jsv_sub_add_param('l_hard', "h_vmem=$qlogin_memory_limit" . "g");
-    # we're done, correct and submit
+    jsv_sub_add_param('l_hard', "h_vmem=$qlogin_mem_limit" . "g");
+    # we're done, correct it and submit it
     if ($debug) {
       jsv_log_info("--- CORRECTED PARAMS ---");
       jsv_show_params();
@@ -71,12 +71,11 @@ jsv_on_verify(sub {
   }
 
   #########################################################################
-  # Jobs with large memory requirements
+  # Jobs requesting memory limits
   #########################################################################
   if (jsv_sub_is_param('l_hard','h_vmem')){
     my $h_vmem = jsv_sub_get_param('l_hard','h_vmem');
-    # large memory requests go to himem node in whole.q
-    # regardless of how many slots were requested
+    # large memory requests go to himem node in whole.q regardless of how many slots were requested
     # remove any memory limits - job has the whole node
     if ($h_vmem =~ s/g$//i){
       if ($h_vmem >= $mem_threshold){
